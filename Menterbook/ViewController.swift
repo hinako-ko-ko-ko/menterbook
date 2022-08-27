b34c9dc80eaf8eb06aa9a83db0455fe8c8066bfe
Hinako Nakai <hinako.0113n@gmail.com>
2022-08-27 20:42:28 +0900

    Initial Commit

---
+++ Menterbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Menterbook
+//
+//  Created by 中井日向子 on 2022/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
